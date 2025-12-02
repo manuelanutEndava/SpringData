5fd6187b030418212eb8cd7af3b4a8c687edb743
manuelanutEndava <Manuela.Nut@endava.com>
2025-12-02 14:40:55 +0200

    Add ex chapter 7

---
+++ Ch7Sp/Ch7SpEx1/Ch7SpEx1/src/main/java/com/example/Ch7SpEx1/controllers/MainController.java
@@ -0,0 +1,13 @@
+package com.example.Ch7SpEx1.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/home")
+    public String home() {
+       return "HomePage.html";
+    }
+}
---
+++ Ch7Sp/Ch7SpEx1/Ch7SpEx1/src/test/java/com/example/Ch7SpEx1/Ch7SpEx1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ch7SpEx1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch7SpEx1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
