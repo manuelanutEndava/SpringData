b46d88384f55450e1c8c73d23fa7a010615d1d57
manuelanutEndava <Manuela.Nut@endava.com>
2025-12-03 14:56:05 +0200

    add ex for chapter 9

---
+++ Ch9Sp/Ch9SpEx3/src/main/java/com/example/Ch9SpEx3Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch9SpEx3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch9SpEx3Application.class, args);
+    }
+
+}
---
+++ Ch9Sp/Ch9SpEx3/src/main/java/com/example/processor/LoginProcessor.java
@@ -0,0 +1,54 @@
+package com.example.processor;
+
+import com.example.service.LoggedUserManagementService;
+import com.example.service.LoginCountService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+@Component
+@RequestScope
+public class LoginProcessor {
+    private final LoggedUserManagementService loggedUserManagementService;
+    private final LoginCountService loginCountService;
+
+    private String username;
+    private String password;
+
+    public LoginProcessor(
+            LoggedUserManagementService loggedUserManagementService,
+            LoginCountService loginCountService){
+        this.loggedUserManagementService = loggedUserManagementService;
+        this.loginCountService = loginCountService;
+    }
+
+    public boolean login() {
+        loginCountService.increment();
+
+        String username = this.getUsername();
+        String password = this.getPassword();
+
+
+boolean loginResult=false;
+        if ("natalie".equals(username) && "password".equals(password)) {
+        loginResult=true;
+        loggedUserManagementService.setUsername(username);
+        }
+        return loginResult;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
